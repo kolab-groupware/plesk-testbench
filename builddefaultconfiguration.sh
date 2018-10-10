a1cf626094ae9a021219782644409f7f21d67374
Christian Mollekopf <chrigi_1@fastmail.fm>
2018-10-10 20:19:58 +0200

    Mount the setup script

--- builddefaultconfiguration.sh
+++ builddefaultconfiguration.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-CONTAINERID=$(docker run --tmpfs /run -v /sys/fs/cgroup:/sys/fs/cgroup:ro -d -v $(pwd)/data:/root/data plesk/centos7:installed)
-docker exec $CONTAINERID /root/setup.sh
+CONTAINERID=$(docker run --rm --tmpfs /run -v $(pwd)/setup.sh:/setup.sh -v /sys/fs/cgroup:/sys/fs/cgroup:ro -d -v $(pwd)/data:/root/data plesk/centos7:installed)
+docker exec $CONTAINERID /setup.sh
 docker commit $CONTAINERID plesk/centos7:configured
 docker stop $CONTAINERID
