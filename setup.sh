25a4a333eb304c4c01a35ef076a3db7f01209291
Christian Mollekopf <mollekopf@kolabsys.com>
2018-10-11 14:28:51 +0200

    Wait for installation to finish before comitting the container
    Otherwise we end up with a randomly broken installation.

--- setup.sh
+++ setup.sh
@@ -1,26 +1,28 @@
 #!/usr/bin/env bash
 
-#Prepare plesk so we can run this from a Dockerfile
 export PATH=/usr/lib/plesk-9.0:/usr/lib64/plesk-9.0:$PATH
+# This will run reconfiguration for a change of ip etc
 psa_service execute_actions
+# Set the admin password
 plesk bin admin --set-password -passwd Welcome2KolabSystems
+# Start all plesk services
 psa_service startall
 
-#Actual setup script starts here
+# Actual setup script starts here
 IP=$(hostname -i)
 
-#Install extensions
+# Install extensions
 cp /root/data/panel.ini /usr/local/psa/admin/conf/
 plesk bin extension -i /root/data/kolab.zip
 cp /root/data/LicenseFaker.php /usr/local/psa/admin/plib/modules/kolab/library/
 chmod 644 /usr/local/psa/admin/plib/modules/kolab/library/LicenseFaker.php
 plesk bin extension -i /root/data/seafile.zip
 
-#Setup default domain
+# Setup default domain
 plesk bin poweruser --on -ip $IP -domain bionic.whd.pxts.ch
 plesk bin poweruser --off
 
-#Create Kolab Reseller with plan
+# Create Kolab Reseller with plan
 plesk bin reseller_plan -c "Kolab Reseller Plan" -ext_permission_kolab_manage_kolab true
 plesk bin reseller --create kolab-reseller -name "Kolab Reseller" -passwd Welcome2KolabSystems -country US -service-plan "Kolab Reseller Plan"
 
@@ -32,5 +34,31 @@ plesk bin service_plan_addon -c "Seafile" -owner kolab-reseller -ext_permission_
 # Kolab Customer with domain
 plesk bin customer --create kolab-customer -name "Kolab Customer" -passwd Welcome2KolabSystems -country US -notify false
 plesk bin subscription --create kolab-customer.maipo.whd.pxts.ch -owner kolab-reseller -service-plan "Kolab Domain" -ip $IP -login kolab-customer -passwd "Welcome2KolabSystems"
+
+
+function waitForServiceState() {
+    while true; do
+        if [ $(systemctl is-active $1) == $2 ]; then
+            break
+        fi
+
+        sleep 1
+    done
+}
+
+# For lack of a better method we wait until the service is started, and assume the setup is complete with that.
+waitForServiceState seafile@8000.service "active"
+waitForServiceState seafile@8005.service "active"
+waitForServiceState seahub@8001.service "active"
+waitForServiceState seahub@8006.service "active"
+
+# Teardown so we can safely commit the container (otherwise we may still have tasks in progress)
+psa_service stopall
+systemctl stop seafile@8000.service
+systemctl stop seafile@8005.service
+systemctl stop seahub@8001.service
+systemctl stop seahub@8006.service
+systemctl stop mariadb.service
+systemctl stop httpd.service
 
 exit 0
